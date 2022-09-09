f0333d87cb8a4096128d561c7cc9a61e5a120f5a
aldysyahrir <aldhyrombe@gmail.com>
2022-09-10 02:01:15 +0800

    Fix Bugs in API getAllFoods

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors")
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(cors("*"));
 app.use(logger('dev'));
 app.use(express.json({limit : '50mb'}));
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +17,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', indexRouter);
-
 
 module.exports = app;
